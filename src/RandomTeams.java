34e106ee8e3cacd0db34cc9733aac3da2272c7fa
imclaren <iainmclaren40@gmail.com>
2018-10-05 13:39:17 -0400

    Initial commit

--- src/RandomTeams.java
+++ src/RandomTeams.java
@@ -1,22 +1,43 @@
+import java.util.Arrays;
+
 public class RandomTeams {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] history = new int[8];
-        teamGen(history);
-    }
-    public static void teamGen(int[] history){
-        //step 1: random num between 1 and 8
-        int team1 = (int)(Math.random()*9) +1;
-        int team2 = (int)(Math.random()*9) +1;
-        while(team2 == team1){
-            team2 = (int)(Math.random()*9) +1;
+        for (int i = 0; i < 4; i++) {
+            teamGen(history);
         }
-        for(int i = 0; i<=history.length-1; i++){
-            if(history[i]==0){
+        System.out.println(Arrays.toString(history));
+    }
+
+    public static void teamGen(int[] history) {
+        //step 1: random num between 1 and 8
+        int team1 = (int) (Math.random() * 8) + 1;
+        for (int i = 0; i < history.length; i++) {
+            while (history[i] == team1) {
+                team1 = (int) (Math.random() * 8) + 1;
+                i = 0;
+            }
+            if (history[i] == 0) {
                 history[i] = team1;
-                history[i+1] = team2;
+                i = history.length;
             }
         }
-        System.out.println(team1 +", " + team2);
+        int team2 = team2(history);
+        System.out.println(team1 + ", " + team2);
+    }
 
+    public static int team2(int[] history) {
+        int team2 = (int) (Math.random() * 8) + 1;
+        for (int i = 0; i < history.length; i++) {
+            while (history[i] == team2) {
+                team2 = (int) (Math.random() * 8) + 1;
+                i = 0;
+            }
+            if (history[i] == 0) {
+                history[i] = team2;
+                i = history.length;
+            }
+        }
+        return team2;
     }
 }
