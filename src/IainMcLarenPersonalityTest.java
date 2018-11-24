f0ebe4d2938982c6649dd697d5585d95399c2bb5
imclaren01 <iainmclaren40@gmail.com>
2018-11-24 16:56:24 -0500

    Commit

--- src/IainMcLarenPersonalityTest.java
+++ src/IainMcLarenPersonalityTest.java
@@ -1,15 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class IainMcLarenPersonalityTest {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-
-        try{
-            System.out.print("Input file name: ");
-        }catch {
+        boolean fileCorrect = false;
+        System.out.print("Input file name: ");
+        while(!fileCorrect){
             String fileIn = input.next();
-        }
+            try{
+                Scanner in = new Scanner(new File(fileIn));
+                fileCorrect = true;
+            }catch (FileNotFoundException f){System.out.println("Invalid file name, try again");}
+            }
         System.out.print("Output file name: ");
         String fileOut=input.next();
+
     }
 }
