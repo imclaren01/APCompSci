e8c4f1b9ce1ace9739f62848fd2b372eb4165313
imclaren <iainmclaren40@gmail.com>
2018-11-24 19:22:31 -0500

    Commit

--- src/IainMcLarenPersonalityTest.java
+++ src/IainMcLarenPersonalityTest.java
@@ -3,19 +3,19 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class IainMcLarenPersonalityTest {
-    public static void main(String[] args){
+    public static void main(String[] args)throws FileNotFoundException{
+        intro();
+    }
+    public static void intro()throws FileNotFoundException{
         Scanner input = new Scanner(System.in);
-        boolean fileCorrect = false;
         System.out.print("Input file name: ");
-        while(!fileCorrect){
-            String fileIn = input.next();
-            try{
-                Scanner in = new Scanner(new File(fileIn));
-                fileCorrect = true;
-            }catch (FileNotFoundException f){System.out.println("Invalid file name, try again");}
-            }
+        File fileIn = new File(input.next());
+        while (!fileIn.exists()){
+            System.out.print("File not found. Try again: ");
+            fileIn = new File(input.nextLine());
+        }
+        Scanner fileScan = new Scanner(fileIn);
+        //System.out.println
         System.out.print("Output file name: ");
-        String fileOut=input.next();
-
     }
 }
