6327d93d50d3d1f6f03b79d9b4e9c8bdb0874039
imclaren <iainmclaren40@gmail.com>
2018-12-14 13:40:06 -0500

    COMMIT

---
+++ src/Ch8Classes/InBackpack.java
@@ -0,0 +1,15 @@
+package Ch8Classes;
+
+public class InBackpack {
+    private int pencils;
+    private int notebooks;
+    private int binders;
+    private boolean lunch;
+    private String color;
+    private String brand;
+    private boolean waterBottle;
+    private boolean hasLaptop;
+    public InBackpack(int pencils, int notebooks, int binders, boolean lunch, String color, String brand, boolean waterBottle, boolean hasLaptop){
+        
+    }
+}
