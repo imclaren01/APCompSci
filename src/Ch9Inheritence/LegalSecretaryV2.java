09b23eed98835420afa415d4f9206882f3f3e2c8
imclaren <iainmclaren40@gmail.com>
2019-02-15 13:42:05 -0500

    COMMIT

--- src/Ch9Inheritence/LegalSecretaryV2.java
+++ src/Ch9Inheritence/LegalSecretaryV2.java
@@ -1,17 +1,8 @@
 package Ch9Inheritence;
 
-public class LegalSecretaryV2 {
-    public int getHours(){
-        return 40; //Works 40 hours
-    }
+public class LegalSecretaryV2 extends SecretaryV2{
     public double getSalary(){
         return 45000; //Get paid $40,000/year
-    }
-    public int getVacationDays(){
-        return 10; //2 weeks paid vacation
-    }
-    public String getVacationForm(){
-        return "yellow"; //use the yellow form
     }
     public void prepLegalDocs(){
         System.out.println("PREPPED");
--- src/Ch9Inheritence/SecretaryV2.java
+++ src/Ch9Inheritence/SecretaryV2.java
@@ -1,18 +1,6 @@
 package Ch9Inheritence;
 
-public class SecretaryV2 {
-    public int getHours(){
-        return 40; //Works 40 hours
-    }
-    public double getSalary(){
-        return 40000; //Get paid $40,000/year
-    }
-    public int getVacationDays(){
-        return 10; //2 weeks paid vacation
-    }
-    public String getVacationForm(){
-        return "yellow"; //use the yellow form
-    }
+public class SecretaryV2 extends EmployeeV2{
     public void takeDictation(){
         System.out.println("Dictation taken");
     }
